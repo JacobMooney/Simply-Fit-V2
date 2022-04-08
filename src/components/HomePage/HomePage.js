66e3104777de1a46cc89d397508a4c6babc5d3c4
JacobMooney <Jacobhmoon@gmail.com>
2022-04-07 20:19:47 -0500

    Restyling home page, settled on design style colors and fonts, mobile layout for homepage almost done and working on larger screens

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,96 +1,80 @@
 import { Link } from "react-router-dom";
-import './HomePage.css';
+import "./HomePage.css";
 
 function HomePage() {
   return (
     <body>
-      <section>
-        <nav>
+      <nav>
+        <div className="navContainer">
           <a className="logo" href to="/home">
             Simply Fit
           </a>
-          <div>
-            <ul>
-              <li>
-                <Link to="/nutrition"> Nutrition</Link>
-              </li>
-              <li>
-                <Link to="/education">Fitness</Link>
-              </li>
-              <li class="nav-item">
-                <Link to="/options">Routine</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </section>
-      <header>
-        <div>
-          <div>
-            <div>
-              <h1>Simplifying Fitness</h1>
-              <p>Fitness For Anyone</p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <Link to="/nutrition">
-                <button>Learn</button>
-              </Link>
-              <Link to="/options">
-                <button>Train</button>
-              </Link>
-            </div>
-          </div>
+          <ul>
+            <li>
+              <Link to="/nutrition"> Nutrition</Link>
+            </li>
+            <li>
+              <Link to="/education">Fitness</Link>
+            </li>
+            <li>
+              <Link to="/options">Routines</Link>
+            </li>
+          </ul>
         </div>
-      </header>
-      <section>
-        <div>
+      </nav>
+      <div className="container">
+        <header>
           <div>
             <div>
-              <h2>NUTRITION</h2>
-              <p>
-                Learn the basics of nutrition, and get a better understanding of
-                how to fuel yourself to reach your goals.
-              </p>
-              <Link to="/nutrition">
-                <button>Get Started!</button>
-              </Link>
+              <div>
+                <h1 className="banner">Simplifying Fitness</h1>
+                <p>For Everyone</p>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div>
-          <div>
             <div>
-              <h2>TRAINING</h2>
-              <p>
-                Explore the ideas and concepts that go into fitness and weight
-                training.
-              </p>
-              <Link to="/education">
-                <button>Get Started!</button>
-              </Link>
+              <div>
+                <Link to="/nutrition">
+                  <button>Education</button>
+                </Link>
+                <Link to="/options">
+                  <button>Training</button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section>
-        <div>
-          <div>
-            <div>
-              <h2>PROGRAM BUILDER</h2>
-              <p>
-                Use our routine maker to find a program for your specific goals.
-              </p>
-              <Link to="/options">
-                <button>Get Started!</button>
-              </Link>
-            </div>
+        </header>
+        <section>
+          <div className="infoCard">
+            <h2>NUTRITION</h2>
+            <p>
+              Learn the basics of nutrition, and get a better understanding of
+              how to fuel yourself to reach your goals.
+            </p>
+            <Link to="/nutrition">
+              <button>Get Started!</button>
+            </Link>
           </div>
-        </div>
-      </section>
+          <div className="infoCard">
+            <h2>TRAINING</h2>
+            <p>
+              Explore the ideas and concepts that go into fitness and weight
+              training.
+            </p>
+            <Link to="/education">
+              <button>Get Started!</button>
+            </Link>
+          </div>
+          <div className="infoCard">
+            <h2>PROGRAM BUILDER</h2>
+            <p>
+              Use our routine maker to find a program for your specific goals.
+            </p>
+            <Link to="/options">
+              <button>Get Started!</button>
+            </Link>
+          </div>
+        </section>
+      </div>
     </body>
   );
 }
